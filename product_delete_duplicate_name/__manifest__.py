f7b942843feb3518d92453f3020db9015074db95
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-12-12 13:39:19 +0100

    product_delete_duplicate

---
+++ product_delete_duplicate_name/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Product Delete Duplicate Name',
+    'version': '18.0.1.0.0',
+    'author': 'Abraham Carrasco (Xtendoo)',
+    'category': 'Custom',
+    'depends': [
+        'base',
+        'product',
+        'stock'
+    ],
+    'data': [
+        "views/product_view.xml",
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ product_delete_duplicate_name/models/product_template.py
@@ -0,0 +1,14 @@
+from odoo import models, api
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.model
+    def remove_duplicate_products(self):
+        products = self.search([])
+        product_names = {}
+        for product in products:
+            if product.name in product_names:
+                product.active = False
+            else:
+                product_names[product.name] = product.id
