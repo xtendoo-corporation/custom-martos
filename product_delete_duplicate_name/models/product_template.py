ae910c9c97c24a3f7b425f937ff116c3102d07a7
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-12-13 11:01:36 +0100

    product_delete_duplicate

--- product_delete_duplicate_name/models/product_template.py
+++ product_delete_duplicate_name/models/product_template.py
@@ -26,8 +26,12 @@ class ProductTemplate(models.Model):
             if product_categ_name.startswith("Todo / "):
                 product_categ_name = product_categ_name.replace("Todo / ", "")
 
+            print("Product category: ", product_categ_name)
+
             product_category = ProductCategory.search([('name', '=', product_categ_name)], limit=1)
             if product_category:
                 product.categ_id = product_category.id
                 print("Product category updated: ", product.name, product_category.name)
+            else:
+                print("Product category not found: ", product.name, product_categ_name)
 
