560ba53a84a5a53cfdf795a0e097804273b0a7f3
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-12-13 11:03:49 +0100

    product_delete_duplicate

--- product_delete_duplicate_name/models/product_template.py
+++ product_delete_duplicate_name/models/product_template.py
@@ -6,17 +6,6 @@ from odoo.auto.addons.sale.models.product_category import ProductCategory
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    # @api.model
-    # def remove_duplicate_products(self):
-    #     products = self.search([])
-    #     product_names = {}
-    #     for product in products:
-    #         print("Product name: ", product.name)
-    #         if product.name in product_names:
-    #             product.active = False
-    #         else:
-    #             product_names[product.name] = product.id
-
     @api.model
     def remove_duplicate_products(self):
         ProductCategory = self.env['product.category']
