3d035488f8cf1514ccff2e4e652ddd1c76100414
Gokac9 <theabraham9@gmail.com>
2024-12-19 09:26:02 +0100

    sync

--- pos_cost_report/models/report_sale_details.py
+++ pos_cost_report/models/report_sale_details.py
@@ -17,17 +17,12 @@ class ReportSaleDetailsCustom(models.AbstractModel):
 
     @api.model
     def get_sale_details(self, date_start=False, date_stop=False, config_ids=False, session_ids=False, **kwargs):
-        # Call the super method to get the original sale details
-        sale_details = super(ReportSaleDetailsCustom, self).get_sale_details(date_start, date_stop, config_ids, session_ids, **kwargs)
+        sale_details = super().get_sale_details(date_start, date_stop, config_ids, session_ids, **kwargs)
         domain = self._get_domain(date_start, date_stop, config_ids, session_ids, **kwargs)
         orders = self.env['pos.order'].search(domain)
 
-        sum_total_cost = 0.0
-        sum_margin = 0.0
-        for order in orders:
-            for order_line in order.lines:
-                sum_total_cost += order_line.total_cost
-                sum_margin += order_line.margin
+        sum_total_cost = sum(order_line.total_cost for order in orders for order_line in order.lines)
+        sum_margin = sum(order_line.margin for order in orders for order_line in order.lines)
 
         sale_details['sum_total_cost'] = sum_total_cost
         sale_details['sum_margin'] = sum_margin
